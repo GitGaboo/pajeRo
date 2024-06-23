985e7776baa07e833e1d41bb5ef9b358e53b554e
GitGaboo <gabrieldonacimento667@gmail.com>
2024-06-23 15:36:12 -0300

    Update procesar.php

--- procesar.php
+++ procesar.php
@@ -1,3 +1,5 @@
 <?php
-madre mia 
+    $usuarios=["email" => "pepito47@gmail.com", "password" => "pepeto23"],
+    ["email" => "alberto1234@gmail.com", "password" => "1234"],
+    ["email" => "mauro1747@gmail.com", "password" => "987654321"]
 ?>
